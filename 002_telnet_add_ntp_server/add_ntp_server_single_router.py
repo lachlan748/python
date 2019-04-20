1c622f063c04093d72379e6491249ec9e4195fd3
lachlanfalconer <lachlanfalconer@mac.com>
2019-04-20 07:21:41 +0000

    Added telnet script for ntp server

---
+++ 002_telnet_add_ntp_server/add_ntp_server_single_router.py
@@ -0,0 +1,24 @@
+import getpass
+import sys
+import telnetlib
+
+HOST = "10.0.0.1"
+user = raw_input("Enter your telnet username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write("enable\n")
+tn.write("cisco\n")
+tn.write("conf t\n")
+tn.write("ntp server 1.1.1.1 prefer\n")
+tn.write("end\n")
+tn.write("exit\n")
+
+print tn.read_all()
