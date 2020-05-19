42a4459ca1e303112a9f01c76d81f62632897ccc
Lachlan Falconer <lachlanfalconer@Lachlans-MBP-15.lan>
2020-05-19 14:20:13 +0100

    Updated main script with getpass

--- cisco_modeling_labs/main.py
+++ cisco_modeling_labs/main.py
@@ -1,9 +1,14 @@
 import urllib3
+import getpass
 from virl2_client import ClientLibrary
 from pprint import pprint
 
+# Get login credentials
+username = input("Enter CML username: ") 
+password = getpass.getpass(prompt = "Enter CML password: ")
+
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-client = ClientLibrary("https://192.168.137.252", "admin", password, ssl_verify=False)
+client = ClientLibrary("https://192.168.137.252", username, password, ssl_verify=False)
 client.wait_for_lld_connected()
 
 for lab in client.all_labs():
