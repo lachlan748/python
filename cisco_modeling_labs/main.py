9445fc1fe12aac5ab7ec777272aaf39fa9a99963
Lachlan Falconer <lachlanfalconer@Lachlans-MBP-15.lan>
2020-05-19 13:53:16 +0100

    Updated main.py

--- cisco_modeling_labs/main.py
+++ cisco_modeling_labs/main.py
@@ -3,12 +3,13 @@ from virl2_client import ClientLibrary
 from pprint import pprint
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-client = ClientLibrary("https://192.168.137.252", "admin", "cakeface99", ssl_verify=False)
+client = ClientLibrary("https://192.168.137.252", "admin", password, ssl_verify=False)
 client.wait_for_lld_connected()
 
 for lab in client.all_labs():
     print("Current labs on CML include:\n")
     print(f"Lab ID {lab.id}, Title: {lab.title}\n")
-    print(f"Starting lab {lab.id}")
-    lab.start()
+    #print(f"Starting lab {lab.id}")
+    #lab.start()
+    lab.stop()
     #pprint(dir(lab))
